40b60142b5bdfb87b5ba5f74140721d8a0cec751
Joseph Mwangi <josymuramu@gmail.com>
2025-03-21 18:35:16 +0300

    completed the arrow functions lab

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(a,b){
+    let result = a / b
+    return result
+}
+console.log(divide(2000,100))
+
+const square = (x) => {
+    let result = x * x
+    return result
+}
+console.log(square(2))
+
+let add = (a,b) => {
+    let result = a + b
+    return result
+}
+console.log(add(3,4))
